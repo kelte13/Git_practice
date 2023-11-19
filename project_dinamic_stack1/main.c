2dce6c02c48c2a336e575571dc4f89e46dae1a83
kelte13 <dim.tcka4ew@gmail.com>
2023-11-19 19:49:31 +0300

    .

---
+++ project_dinamic_stack1/main.c
@@ -0,0 +1,13 @@
+#include "stack.h"
+
+int main()
+{
+    push(4);
+    push(7);
+    show();
+    pop();
+    printf("\n");
+    show();
+
+    return 0;
+}
---
+++ project_dynamik_stack1.2/head.h
@@ -0,0 +1,10 @@
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+#include <stdbool.h>
+
+void show(void);
+bool is_empty(void);
+int push(int n);
+int pop(void);
+
+#endif // STACK_H_INCLUDED
